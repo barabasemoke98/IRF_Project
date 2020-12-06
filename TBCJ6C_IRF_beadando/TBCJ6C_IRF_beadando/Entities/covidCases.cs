028f38c7f4d3bbeb55e85c37defe747444e6598c
Emőke Edit Barabás <barabas.emoke98@gmail.com>
2020-12-06 21:30:58 +0100

    1

---
+++ TBCJ6C_IRF_beadando/TBCJ6C_IRF_beadando/Entities/covidCases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBCJ6C_IRF_beadando.Entities
+{
+    public class covidCases
+    {
+        public int day { get; set; }
+        public int month { get; set; }
+        public int year { get; set; }
+        public int cases { get; set; }
+        public int death { get; set; }
+        public string country { get; set; }
+        public string continent { get; set; }
+
+    }
+}
