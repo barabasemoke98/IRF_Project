7e0bc1980523a0e6c8975ed032f386e9ea8f6e07
Emőke Edit Barabás <barabas.emoke98@gmail.com>
2020-12-06 21:45:58 +0100

    2

--- TBCJ6C_IRF_beadando/TBCJ6C_IRF_beadando/Form1.cs
+++ TBCJ6C_IRF_beadando/TBCJ6C_IRF_beadando/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TBCJ6C_IRF_beadando.Entities;
+using System.IO;
 
 namespace TBCJ6C_IRF_beadando
 {
     public partial class Form1 : Form
     {
+        List<covidCases> allCovidCases = new List<covidCases>(); 
         public Form1()
         {
             InitializeComponent();
+            StreamReader sr = new StreamReader(@"\covid.csv", Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine().Split(';');
+                allCovidCases.Add(new covidCases()
+                {
+                    day = int.Parse(line[0]),
+                    month = int.Parse(line[1]),
+                    year = int.Parse(line[2]),
+                    cases = int.Parse(line[3]),
+                    death = int.Parse(line[4]),
+                    country = line[5],
+                    continent = line[6]
+                });
+            }
         }
     }
 }
