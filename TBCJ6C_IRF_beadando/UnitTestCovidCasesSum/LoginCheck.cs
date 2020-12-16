898ff2e2ffe924dfd5d4cf4263e636d1d9f17fd8
Emőke Edit Barabás <barabas.emoke98@gmail.com>
2020-12-16 14:17:27 +0100

    11

---
+++ TBCJ6C_IRF_beadando/UnitTestCovidCasesSum/LoginCheck.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBCJ6C_IRF_beadando.Entities;
+
+namespace UnitTestCovidCasesSum
+{
+    public class LoginCheck
+    {
+
+
+        public void TestLogin(string username, string password, bool permission)
+        {
+            var loginform = new Login();
+
+            var actualResult = loginform.userData(username, password);
+
+            Assert.AreEqual(username, actualResult.userName);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreEqual(permission, actualResult.Permission);
+        }
+    }
+}
