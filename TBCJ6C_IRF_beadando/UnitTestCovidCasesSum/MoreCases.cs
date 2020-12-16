0303591ce777b75700edfaabbcd4abb691f570ae
Emőke Edit Barabás <barabas.emoke98@gmail.com>
2020-12-16 12:05:11 +0100

    8

--- TBCJ6C_IRF_beadando/UnitTestCovidCasesSum/MoreCases.cs
+++ TBCJ6C_IRF_beadando/UnitTestCovidCasesSum/MoreCases.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TBCJ6C_IRF_beadando;
 
 namespace UnitTestCovidCasesSum
 {
     class MoreCases
     {
+        [Test]
+        public void SumCases(int c1sum, int c2sum, string expectedResult)
+        {
+            var ccform = new CovidCasesForm();
+            var ActualResult = ccform.MoreCases(c1sum, c2sum);
+
+            Assert.AreEqual(expectedResult, ActualResult);
+        }
     }
 }
